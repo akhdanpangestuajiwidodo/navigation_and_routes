8e4af6a721f97e0911759cf309009928f6d34a5e
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-01-27 07:52:12 +0700

    Back button

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Kembali'),
+        ),
+      ),
+    );
   }
 }
