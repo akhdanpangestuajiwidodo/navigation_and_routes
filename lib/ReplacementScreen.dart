b5d930a5fe0c637b5b95a433b9883e12d7e1da62
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-01-27 07:36:36 +0700

    Initial some screen types of second screen

---
+++ lib/ReplacementScreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ReplacementScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
---
+++ lib/second_screen_with_data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SecondScreenWithData extends StatelessWidget{
+
+  final String data;
+
+  SecondScreenWithData({required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+  
+}
